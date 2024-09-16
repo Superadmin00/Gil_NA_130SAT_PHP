ddc6274e76af52513592e7fef6ab5c0d0f511f8e
Your Name <you@example.com>
2024-09-16 20:44:41 +0800

    0921_13_eager-loading-and-the-n-plus-1-problem

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -1,14 +1,18 @@
 <x-layout>
     <x-slot:heading>Job Listing</x-slot:heading>
 
-    <ul>
+    <div class="space-y-4">
         @foreach ($jobs as $job)
-        <li>
-            <a href="/jobs/{{$job['id']}}" class="hover:underline hover:text-blue-700">
-                <strong>{{$job['title']}}</strong>: Pays {{$job['salary']}} per year.
+            <a href="/jobs/{{ $job['id'] }}"
+                class="block px-4 py-6 border border-gray-300 rounded-lg hover:bg-gray-600 hover:text-white">
+                <div class="font-bold text-blue-400">
+                    {{ $job->employer->name }}
+                </div>
+                <div>
+                    <strong>{{ $job['title'] }}</strong>: Pays {{ $job['salary'] }} per year.
+                </div>
             </a>
-        </li>
         @endforeach
-    </ul>
+    </div>
 
 </x-layout>

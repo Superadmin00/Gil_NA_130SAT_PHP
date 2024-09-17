efdf5e14ca8ee3292f6757ffd658e5378e65380c
Your Name <you@example.com>
2024-09-17 11:51:18 +0800

    0921_14_pagination

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -3,16 +3,21 @@
 
     <div class="space-y-4">
         @foreach ($jobs as $job)
-            <a href="/jobs/{{ $job['id'] }}"
-                class="block px-4 py-6 border border-gray-300 rounded-lg hover:bg-gray-600 hover:text-white">
-                <div class="font-bold text-blue-400">
-                    {{ $job->employer->name }}
-                </div>
-                <div>
-                    <strong>{{ $job['title'] }}</strong>: Pays {{ $job['salary'] }} per year.
-                </div>
-            </a>
+        <a href="/jobs/{{ $job['id'] }}"
+            class="block px-4 py-6 border border-gray-300 rounded-lg hover:bg-gray-600 hover:text-white">
+            <div class="font-bold text-blue-400">
+                {{ $job->employer->name }}
+            </div>
+            <div>
+                <strong>{{ $job['title'] }}</strong>: Pays {{ $job['salary'] }} per year.
+            </div>
+        </a>
         @endforeach
+
+        <div>
+            {{ $jobs->links() }}
+        </div>
+
     </div>
 
 </x-layout>

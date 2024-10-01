d3472a4a2412926b340df8921aad2b4c056f1de5
Your Name <you@example.com>
2024-10-01 14:49:19 +0800

    14. Superglobals and Current Page Styling

--- views/about.view.php
+++ views/about.view.php
@@ -1,13 +1,11 @@
 <?php require('partials/head.php') ?>
 <?php require('partials/nav.php') ?>
-
 <?php require('partials/banner.php') ?>
 
-  <main>
-    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <!-- Your content -->
-      <h1>You are in the About Page</h1>
+<main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <p>Hello. Welcome to the about page.</p>
     </div>
-  </main>
+</main>
 
-  <?php require('partials/footer.php') ?>
+<?php require('partials/footer.php') ?>

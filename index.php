e9460f2191fa151780b944615fe1dc17962e7f5e
Your Name <you@example.com>
2024-10-01 16:30:46 +0800

    18. Extract a PHP Database Class

--- index.php
+++ index.php
@@ -1,17 +1,10 @@
 <?php
 
 require 'functions.php';
+//require 'router.php';
+require 'Database.php';
 
-// Connect to the MySQL database.
-$dsn = "mysql:host=localhost;port=3306;dbname=php4beginnersdb;user=root;charset=utf8mb4";
+$db = new Database();
+$posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
 
-$pdo = new PDO($dsn);
-
-$statement = $pdo->prepare("select * from posts");
-$statement->execute();
-
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-foreach ($posts as $post) {
-    echo "<li>" . $post['title'] . "</li>";
-}
+dd($posts);

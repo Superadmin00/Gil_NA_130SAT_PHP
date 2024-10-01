0650f4757a1e869381d75a71161f53ff995b4b68
Your Name <you@example.com>
2024-10-01 14:34:51 +0800

    13. PHP Partials

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -1,14 +1,4 @@
-<!doctype html>
-<html lang="en" class="h-full bg-gray-100">
-<head>
-    <meta charset="UTF-8">
-    <title>PHP For Beginners</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
-
-<body class="h-full">
-<div class="min-h-full">
-  <nav class="bg-gray-800">
+<nav class="bg-gray-800">
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div class="flex h-16 items-center justify-between">
         <div class="flex items-center">
@@ -114,18 +104,3 @@
       </div>
     </div>
   </nav>
-
-  <header class="bg-white shadow">
-    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold tracking-tight text-gray-900">About Us</h1>
-    </div>
-  </header>
-  <main>
-    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <!-- Your content -->
-      <h1>You are in the About Page</h1>
-    </div>
-  </main>
-</div>
-</body>
-</html>

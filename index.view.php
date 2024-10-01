565bb428ad062fc971f35990d0abe50ee484b0e7
Your Name <you@example.com>
2024-10-01 12:55:02 +0800

    10. Separate Logic From the Template

---
+++ index.view.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP For Beginners</title>
+</head>
+
+<body>
+    <ul>
+        <?php foreach ($filteredBooks as $book) : ?>
+            <li>
+                <a href="<?= $book['purchaseUrl'] ?>">
+                    <?= $book['name']; ?> (<?= $book['releaseYear'] ?>) - By <?= $book['author'] ?>
+                </a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</body>
+</html>

ac9feefcef540e70f17bc127f4e9540094535121
Your Name <you@example.com>
2024-10-01 16:22:39 +0800

    17. PDO First Steps

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
+
+// Connect to the MySQL database.
+$dsn = "mysql:host=localhost;port=3306;dbname=php4beginnersdb;user=root;charset=utf8mb4";
+
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("select * from posts");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['title'] . "</li>";
+}

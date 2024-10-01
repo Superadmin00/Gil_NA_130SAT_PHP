408af1d90660fffda9d4a3fec349b3a59b205634
Your Name <you@example.com>
2024-10-01 12:41:11 +0800

    3. Your First PHP Tag

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP For Beginners</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            echo "Hello Word!";
+        ?>
+    </h1>
+</body>
+</html>

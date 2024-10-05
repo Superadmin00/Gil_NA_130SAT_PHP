a351e8be2cef985ed4d7fb4b4e2e01fdeb87ee77
Your Name <you@example.com>
2024-10-05 15:13:52 +0800

    30. PHP Autoloading and Extraction

--- Core/Database.php
+++ Core/Database.php
@@ -17,23 +17,30 @@ class Database
     public function query($query, $params = [])
     {
         $this->statement = $this->connection->prepare($query);
+
         $this->statement->execute($params);
+
         return $this;
     }
+
     public function get()
     {
         return $this->statement->fetchAll();
     }
+
     public function find()
     {
         return $this->statement->fetch();
     }
+
     public function findOrFail()
     {
         $result = $this->find();
+
         if (! $result) {
             abort();
         }
+
         return $result;
     }
 }
